a5e77457e0b72315c47d7ec69ec6a30d949075c6
PIGNAL <joni_ballatore@hotmail.com>
2020-10-02 17:54:14 -0300

    Primer Commit

---
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filtrosValidos, setFiltro } from '../filtro/filtro.actions';
+import { limpiar } from '../todo.actions';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styleUrls: ['./todo-footer.component.css']
+})
+export class TodoFooterComponent implements OnInit {
+
+  filtroActual: filtrosValidos = 'todos';
+  filtros: filtrosValidos[] = ['todos', 'completados', 'pendientes' ];
+
+  pendientes = 0;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+
+    this.store.subscribe(state => {
+      this.filtroActual = state.filtro;
+      this.pendientes = state.todos.filter(todo => !todo.completado).length;
+    });
+  }
+
+  cambiarFiltro(filtro: filtrosValidos){
+    this.store.dispatch(setFiltro({filtro}));
+  }
+
+  LimpiarCompletados(){
+    this.store.dispatch(limpiar());
+  }
+}
